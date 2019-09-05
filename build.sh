e24d72ba722f2c1a2be04dc7808c44ae3f6dc234
Samuel Berthe <dev@samuel-berthe.fr>
2019-09-05 20:47:14 +0200

    release of version 0.2.0

--- build.sh
+++ build.sh
@@ -5,7 +5,9 @@ set -e
 
 moodle-plugin-ci install --moodle /moodle --data /data --branch MOODLE_37_STABLE --db-type pgsql --db-user moodle --db-pass moodle --db-name moodel --db-host postgres
 
-moodle-plugin-ci grunt
+cd /moodle/local/keyboard_shortcuts/amd
+rm -rf build/
+grunt amd
 
-rm -rf amd/build/
-cp -r /moodle/local/keyboard_shortcuts/amd/build/ amd/build/
+rm -rf /plugin/amd/build/
+cp -r build/ /plugin/amd/build/
